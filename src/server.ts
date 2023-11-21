83446bb789672c166096b10a25f8bad686915d5b
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-11-20 21:59:43 -0300

    fix: correcting configs to deploy in render

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,10 @@ import { env } from './env'
 
 const start = async () => {
   try {
-    await app.listen({ port: env.PORT })
+    await app.listen({
+      port: env.PORT,
+      host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    })
     console.log('HTTP Server running in http://localhost:3333')
   } catch (err) {
     app.log.error(err)
